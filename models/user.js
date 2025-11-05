3493282c0f4efd237ca2800ab9052799c6ea08f9
e-khay <samuelamepre2@gmail.com>
2025-11-05 04:58:06 +0100

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verified: { type: Boolean, default: false },
+  resetToken: { type: String },
+  resetTokenExpire: { type: Date },
+});
+
+export default mongoose.model("User", userSchema);
